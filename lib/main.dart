33bd2c09d970bc796a33c3b28393fd0eab347e71
Alain Gauthier Ndamwey Djamba <alain.djamba@ifood.com.br>
2024-09-19 00:25:08 -0300

    fix: fix firebase configurations for ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:iyakking/pages/auth.dart';
 import 'package:iyakking/pages/auth_or_app_page.dart';
-// import 'package:iyakking/pages/loading_page.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
 
--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,26 +1,42 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:iyakking/models/yakking_user.dart';
 import 'package:iyakking/pages/auth.dart';
 import 'package:iyakking/pages/loading_page.dart';
 import 'package:iyakking/pages/yak_page.dart';
 import 'package:iyakking/services/auth/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
+  Future<void> init(BuildContext context) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-          stream: AuthService().userChanges,
-          builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return LoadingPage();
-            } else {
-              return snapshot.hasData ? YakPage() : Auth();
-            }
-          }),
-    );
+    return FutureBuilder(
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingPage();
+          } else {
+            return StreamBuilder<YakkingUser?>(
+                stream: AuthService().userChanges,
+                builder: (ctx, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return LoadingPage();
+                  } else {
+                    return snapshot.hasData ? YakPage() : Auth();
+                  }
+                });
+          }
+        },
+        future: init(context));
   }
 }
